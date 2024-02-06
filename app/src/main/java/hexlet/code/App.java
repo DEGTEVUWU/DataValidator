aa965c648af487ad5b77fc18b134c55bac44fcc8
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-06 11:58:26 +0200

    fix App.class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
-public static String testMethod(String test) {
-    return test;
-}
+    public static String testMethod(String test) {
+
+        return test;
+    }
 }
