f31be132abf211a4d74e5690506e09e1b5dd2e88
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-06 17:24:35 +0200

    added two new classes - Validator and StringSchema

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,38 @@
+package hexlet.code.schemas;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class StringSchema {
+    private boolean required = false;
+    private String containsString = "";
+
+    public StringSchema required() {
+        this.required = true;
+        return this;
+    }
+
+    public StringSchema contains(String chars) {
+        this.containsString = chars;
+        return this;
+    }
+
+    public boolean isValid(String str) {
+        StringSchema schema = new StringSchema();
+        schema.required = this.required;
+        schema.containsString = this.containsString;
+
+        if (required && (str == null || str.isEmpty())) {
+            return false;
+        }
+        if (!containsString.isEmpty()) {
+            return str.contains(containsString);
+        }
+        return true;
+    }
+}
