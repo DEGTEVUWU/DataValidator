9be754ffcd641c24b8b402e715a419c11df27f9f
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-06 21:33:12 +0200

    clean main method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,35 +3,5 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello!");
-
-        System.out.println("");
-
-        var v = new Validator();
-        var schema = v.string();
-
-        System.out.println(schema.isValid(""));
-        System.out.println(schema.isValid(null));
-        schema.required();
-        System.out.println(schema.isValid(null));
-        System.out.println(schema.isValid(""));
-/*
-        System.out.println("");
-        System.out.println(schema.isValid("Hello!"));
-        System.out.println(schema.contains("He!!"));
-        System.out.println(schema.contains("whatthe").isValid("what does the fox say"));
-        System.out.println(schema.isValid("what does the fox say"));
-        System.out.println("");
-
-
- */
-        System.out.println("");
-
-        System.out.println(schema.contains("wh").isValid("what does the fox say")); // true
-        System.out.println(schema.contains("what").isValid("what does the fox say")); // true
-        System.out.println(schema.contains("whatthe").isValid("what does the fox say")); // false
-        System.out.println(schema.isValid("what does the fox say"));
-        System.out.println(schema.contains("wh").isValid("what"));
-
     }
 }
