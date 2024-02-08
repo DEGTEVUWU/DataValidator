4ccf4d954cba726b15663a6c0ee0c5ba0e3c9d99
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-08 14:41:12 +0200

    fix linter`s errors from App.class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
     public static void main(String[] args) {
 
