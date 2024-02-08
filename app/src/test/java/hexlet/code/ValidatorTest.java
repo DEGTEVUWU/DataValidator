675e4dc974cdc2304c1cb6485069c9b9c6b08987
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-02-08 14:14:45 +0200

    create new test

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,6 +44,62 @@ class ValidatorTest {
         assertThat(actual1.getAllowPositive()).isEqualTo(expected2);
         assertThat(actual1.getMinRange()).isEqualTo(expected3);
         assertThat(actual1.getMaxRange()).isEqualTo(expected4);
+    }
+
+    @Test
+    void mapMethodTest() {
+        var baseSchemaTestObj = new Validator();
+        var actual1 = new Validator().map();
+        actual1.setAllowNull(true);
+        actual1.setAllowShape(true);
+        actual1.setQuantityPair(3);
+
+        var mapWithSchema = new HashMap<String, BaseSchema<String>>();
+        mapWithSchema.put("key1", baseSchemaTestObj.string().required());
+        mapWithSchema.put("key2", baseSchemaTestObj.string().required().contains("val"));
+
+        var expected1 = true;
+        var expected2 = true;
+        var expected3 = 3;
+
+        assertThat(actual1.isAllowNull()).isEqualTo(expected1);
+        assertThat(actual1.isAllowShape()).isEqualTo(expected2);
+        assertThat(actual1.getQuantityPair()).isEqualTo(expected3);
+    }
+    @Test
+    void mapMethodTestWithSchemas() {
+        var v = new Validator();
+        var schema = v.map();
+
+        Map<String, BaseSchema<String>> schemas = new HashMap<>();
+        schemas.put("firstName", v.string().required());
+        schemas.put("lastName", v.string().required().minLength(2));
+
+        schema.shape(schemas);
+
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+
+        var actual1 = schema.isValid(human1);
+        var actual2 = schema.isValid(human2);
+        var actual3 = schema.isValid(human3);
+
+        var expected1 = true;
+        var expected2 = false;
+        var expected3 = false;
+
+        assertThat(actual1).isEqualTo(expected1);
+        assertThat(actual2).isEqualTo(expected2);
+        assertThat(actual3).isEqualTo(expected3);
 
 
     }
